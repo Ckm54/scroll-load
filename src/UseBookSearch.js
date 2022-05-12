8da5dddafa6a8afe2aaa12b4f541c2f5f43a02a1
Collins <mutaic84@gmail.com>
2022-05-12 12:08:22 +0300

    clear list when user changes search query

--- src/UseBookSearch.js
+++ src/UseBookSearch.js
@@ -9,6 +9,10 @@ export default function UseBookSearch(query, pageNumber) {
   const [hasMore, setHasMore] = useState(false)
 
   useEffect(() => {
+    setBooks([])
+  }, [query])
+
+  useEffect(() => {
     setLoading(true)
     setError(false)
     let cancel;
