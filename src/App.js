2a4574bdd552f0c84f6c41676921f4ebe94f76de
Collins <mutaic84@gmail.com>
2022-05-12 11:45:51 +0300

    handle using search query to get results

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import React, { useState } from "react"
 import './App.css';
+import UseBookSearch from './UseBookSearch';
 
 function App() {
-  
+  const [query, setQuery] = useState('t')
+  const [pageNumber, setPageNumber] = useState(1)
+  UseBookSearch(query, pageNumber)
+
+  function handleSearch(e) {
+    setQuery(e.target.value)
+    setPageNumber(1)
+  }
   return (
     <>
-      <input type="text" />
+      <input type="text" onChange={handleSearch}/>
       <div>Title</div>
       <div>Title</div>
       <div>Title</div>
