68d5a45bc239e9d0980850b5e199cbbd6edd47b4
Collins <mutaic84@gmail.com>
2022-05-12 12:19:12 +0300

    useRef and useCallback to determine the last element in the page for pagination

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React, { useState } from "react"
+import React, { useState, useRef, useCallback } from "react"
 import './App.css';
 import UseBookSearch from './UseBookSearch';
 
 function App() {
   const [query, setQuery] = useState('t')
   const [pageNumber, setPageNumber] = useState(1)
+
+  const observer = useRef()
+  const lastBookElement = useCallback(node => {
+    console.log(node)
+  })
+
   const{books,
         hasMore,
         loading,
         error } = UseBookSearch(query, pageNumber)
+  
 
   function handleSearch(e) {
     setQuery(e.target.value)
@@ -17,7 +24,13 @@ function App() {
   return (
     <>
       <input type="text" value={query} onChange={handleSearch}/>
-      {books.map(book => <div key={book}>{book}</div>)}
+      {books.map((book, index) => {
+        if(books.length === index + 1){
+          return <div ref={lastBookElement} key={book}>{book}</div>
+        } else {
+          return <div key={book}>{book}</div>
+        }
+      })}
       <div>{loading && "Loading....."}</div>
       <div>{error && "Error"}</div>
     </>
