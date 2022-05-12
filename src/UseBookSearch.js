4559fd659f82df54daa469ee0be3b705be506120
Collins <mutaic84@gmail.com>
2022-05-12 11:49:02 +0300

    create a cancelToken

--- src/UseBookSearch.js
+++ src/UseBookSearch.js
@@ -4,13 +4,16 @@ import axios from 'axios'
 export default function UseBookSearch(query, pageNumber) {
 
   useEffect(() => {
+    let cancel;
     axios({
       method: "GET",
       url: `http://openlibrary.org/search.json`,
-      params: {q: query, page: pageNumber}
+      params: {q: query, page: pageNumber},
+      cancelToken: new axios.CancelToken(c => cancel = c)
     }).then(res => {
       console.log(res.data)
     })
+    return () => cancel()
   }, [query, pageNumber])
   return null
 }
